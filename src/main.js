0877130d6ee994be52b481ce217a6788a8fe0ce2
watermelonhater <watermelonhater33@gmail.com>
2025-08-01 22:28:54 +0300

    first large commit with a 7in1 structure

--- src/main.js
+++ src/main.js
@@ -1,5 +1,1 @@
-import './style.scss'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-import './reset.css'
+import './scss/main.scss'
